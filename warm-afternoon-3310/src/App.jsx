ad7c7f66ee75b04eddad8187d97ce13a921d9071
Manish <maknish1993@gmail.com>
2023-01-18 15:36:58 +0530

    Auth page created

--- warm-afternoon-3310/src/App.jsx
+++ warm-afternoon-3310/src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import './App.css';
-import Auth from "./pages/authentication/Auth";
-// import SignIn from "../src/pages/authentication/SignIn";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Auth />
-    </>
+    
   )
 }
 
