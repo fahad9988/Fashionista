0d3567369c7a55ecc13cc551b6b4cff004b6be65
fahad9988 <fahad20830@gmail.com>
2023-01-18 15:39:03 +0530

    added

--- warm-afternoon-3310/src/App.jsx
+++ warm-afternoon-3310/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import AllRoutes from './all-routes/AllRoutes';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
