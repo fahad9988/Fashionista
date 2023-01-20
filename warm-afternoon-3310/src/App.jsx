dd872c1708eec3a865991cd0a17e54066bbf9504
Manish <maknish1993@gmail.com>
2023-01-20 22:09:36 +0530

    Admin login-register-page_added

--- warm-afternoon-3310/src/App.jsx
+++ warm-afternoon-3310/src/App.jsx
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom";
 import Footer from './components/Akshay/Footer';
 import Navbar from './components/Akshay/Navbar';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div >
+      <ToastContainer theme='colored' position='top-center'></ToastContainer>
       <Auth0Provider
         domain="dev-qe8rba52yh4bo8bp.us.auth0.com"
         clientId="hr1AAaFk3mX6DCAk0iCQDome4xhe2AwL"
